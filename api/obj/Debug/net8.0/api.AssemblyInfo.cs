358a1aa77989bb1171e1cb51269d6d595fc1948a
Sean <seangrant118@gmail.com>
2024-10-08 13:24:18 -0400

    chore: add dotnet frameworks

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0b75e9c457d0d19b1c83413ce0818906bef213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b65b97582df47bdc7ddd9605c7af60c4b689fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
