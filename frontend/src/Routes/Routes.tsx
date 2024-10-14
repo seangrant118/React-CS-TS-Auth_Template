d5cabcb4a2a88b685f46ae49720c0f0a3e317886
Sean <seangrant118@gmail.com>
2024-10-14 14:00:22 -0400

    feat: wire up login and register pages

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -4,16 +4,20 @@ import HomePage from "../Pages/HomePage/HomePage";
 import PageOne from "../Pages/PageOne/PageOne";
 import PageTwo from "../Pages/PageTwo/PageTwo";
 import PageThree from "../Pages/PageThree/PageThree";
+import LoginPage from "../Pages/LoginPage/LoginPage";
+import RegisterPage from "../Pages/RegisterPage/RegisterPage";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children:[
-            {path: "", element: <HomePage />},
-            {path: "/pageone", element: <PageOne />},
-            {path: "/pagetwo", element: <PageTwo />},
-            {path: "/pagethree", element: <PageThree />},
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+      { path: "/pageone", element: <PageOne /> },
+      { path: "/pagetwo", element: <PageTwo /> },
+      { path: "/pagethree", element: <PageThree /> },
+    ],
+  },
+]);
